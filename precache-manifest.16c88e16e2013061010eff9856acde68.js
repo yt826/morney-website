36f8acbf6a4755147e110035a8cb16e7e443c462
yangtao <1772492146@qq.com>
2020-12-15 19:16:24 +0800

    deploy

---
+++ precache-manifest.16c88e16e2013061010eff9856acde68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ce41ed6553cd70abed",
+    "url": "/morney-website/css/app.0e696a46.css"
+  },
+  {
+    "revision": "2882281e47fbcf3f4f1d7b6979beeaed",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "32ce41ed6553cd70abed",
+    "url": "/morney-website/js/app.cbd83f13.js"
+  },
+  {
+    "revision": "77f0192b5ff00231586b",
+    "url": "/morney-website/js/chunk-vendors.fa950037.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
